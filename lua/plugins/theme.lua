2bbedf6856d7ab5d9e66cc3a0b838871c833e317
Bryan Gonzales Vega <bgonzales.vega@gmail.com>
2023-04-23 18:08:49 -0500

    Update initial theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,26 +3,30 @@ return {
 	lazy = false,
 	priority = 1000,
 	dependencies = {
-		{
-			"catppuccin/nvim",
-			name = "catppuccin",
-		},
-		{
-			"folke/tokyonight.nvim",
-			opts = { style = "moon" },
-		}
+		"catppuccin/nvim",
+		"folke/tokyonight.nvim",
+		"ellisonleao/gruvbox.nvim"
 	},
 	config = function()
-		require('auto-dark-mode').setup {
-			set_dark_mode = function()
-				vim.api.nvim_set_option('background', 'dark')
-				vim.cmd([[colorscheme tokyonight]])
-			end,
-			set_light_mode = function()
-				vim.api.nvim_set_option('background', 'light')
-				vim.cmd([[colorscheme xcodelight]])
-			end,
-		}
-		require("auto-dark-mode").init()
+		if vim.loop.os_uname().sysname == "Darwin" then
+			require('auto-dark-mode').setup {
+				set_dark_mode = function()
+					vim.api.nvim_set_option('background', 'dark')
+					vim.cmd([[colorscheme tokyonight]])
+				end,
+				set_light_mode = function()
+					vim.api.nvim_set_option('background', 'light')
+					vim.cmd([[colorscheme xcodelight]])
+				end,
+			}
+			require("auto-dark-mode").init()
+		elseif vim.loop.os_uname().sysname == "Linux" then
+			vim.api.nvim_set_option('background', 'dark')
+			vim.cmd([[colorscheme catppuccin]])
+		else
+			vim.api.nvim_set_option('background', 'light')
+			vim.cmd([[colorscheme gruvbox]])
+		end
 	end
 }
+
