a1d7fa207572989af8463a2fbb7fb4c2d415cbb5
Bryan Gonzales Vega <bgonzales.vega@gmail.com>
2023-04-23 17:40:43 -0500

    Test theme by operating system

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -10,19 +10,26 @@ return {
 		{
 			"folke/tokyonight.nvim",
 			opts = { style = "moon" },
-		}
+		},
 	},
 	config = function()
-		require('auto-dark-mode').setup {
-			set_dark_mode = function()
-				vim.api.nvim_set_option('background', 'dark')
-				vim.cmd([[colorscheme tokyonight]])
-			end,
-			set_light_mode = function()
-				vim.api.nvim_set_option('background', 'light')
-				vim.cmd([[colorscheme xcodelight]])
-			end,
-		}
-		require("auto-dark-mode").init()
+		if vim.loop.os_uname().sysname == "Darwin" then
+			require('auto-dark-mode').setup {
+				set_dark_mode = function()
+					vim.api.nvim_set_option('background', 'dark')
+					vim.cmd([[colorscheme tokyonight]])
+				end,
+				set_light_mode = function()
+					vim.api.nvim_set_option('background', 'light')
+					vim.cmd([[colorscheme xcodelight]])
+				end,
+			}
+			require("auto-dark-mode").init()
+		elseif vim.loop.os_uname().sysname == "Linux" then
+			vim.api.nvim_set_option('background', 'dark')
+			vim.cmd([[colorscheme catppuccin]])
+		else
+			vim.cmd([[colorscheme catppuccin]])
+		end
 	end
 }
