f051d107c39ca9c3f588c1615355a7fa8958bd8e
Bryan Gonzales Vega <bgonzales.vega@gmail.com>
2023-04-22 23:52:04 -0500

    Update ToDo plugin

--- lua/plugins/todos.lua
+++ lua/plugins/todos.lua
@@ -4,8 +4,6 @@ return {
 	event = { "BufReadPost", "BufNewFile" },
 	config = true,
 	keys = {
-		{ "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
-		{ "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
-		{ "<leader>ft", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+		{ "<leader>ft", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME,NOTE<cr>", desc = "[F]ind [T]o do" },
 	},
 }
