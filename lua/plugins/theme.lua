80d583abb3caf70ce007bc4afd415543f1b26119
Bryan Gonzales Vega <bgonzales.vega@gmail.com>
2023-04-29 15:47:51 -0500

    Update light theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -16,7 +16,7 @@ return {
 				end,
 				set_light_mode = function()
 					vim.api.nvim_set_option('background', 'light')
-					vim.cmd([[colorscheme xcodelight]])
+					vim.cmd([[colorscheme xcodelighthc]])
 				end,
 			}
 			require("auto-dark-mode").init()
